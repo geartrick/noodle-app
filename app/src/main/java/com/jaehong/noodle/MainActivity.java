f39202dc07838c96e691356930f76c19bd21463f
geartrick <leejh4588@naver.com>
2020-06-15 21:07:47 +0900

    inital commit

--- app/src/main/java/com/jaehong/noodle/MainActivity.java
+++ app/src/main/java/com/jaehong/noodle/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jaehong.noodle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b= findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NoodleSelect.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/jaehong/noodle/NoodleSelect.java
@@ -0,0 +1,13 @@
+package com.jaehong.noodle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NoodleSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.noodle_selcet);
+    }
+}
