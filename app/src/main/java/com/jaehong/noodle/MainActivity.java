5713862f019fbfeb7fa4e35622e2c6ee92781ee6
geartrick <leejh4588@naver.com>
2020-06-16 16:32:04 +0900

    inital commit

--- app/src/main/java/com/jaehong/noodle/MainActivity.java
+++ app/src/main/java/com/jaehong/noodle/MainActivity.java
@@ -1,10 +1,16 @@
 package com.jaehong.noodle;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,NoodleSelect.class);
                 startActivity(intent);
+                finish();
             }
         });
 
     }
+
+
+
 }
--- app/src/main/java/com/jaehong/noodle/NoodleSelect.java
+++ app/src/main/java/com/jaehong/noodle/NoodleSelect.java
@@ -1,13 +1,43 @@
 package com.jaehong.noodle;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class NoodleSelect extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.noodle_selcet);
+
+
+        LinearLayout button1 = (LinearLayout)findViewById(R.id.btn1);
+        LinearLayout button2 = (LinearLayout)findViewById(R.id.btn2);
+        LinearLayout button3 = (LinearLayout)findViewById(R.id.btn3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new FragmentA()).commit();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new FragmentB()).commit();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new FragmentC()).commit();
+            }
+        });
     }
 }
